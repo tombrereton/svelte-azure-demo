6f7a89548fb9ea29ffdf760392628bd174ddd11d
Tom Brereton <tom.brereton@exify.net>
2023-09-11 11:48:58 +1000

    add endpoint to send event

--- web/src/routes/backend/add/+server.ts
+++ web/src/routes/backend/add/+server.ts
@@ -1,7 +1,31 @@
 import { json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
+import { EventGridPublisherClient, AzureKeyCredential } from "@azure/eventgrid";
+import { TEEITUP_TOPIC_ENDPOINT, TEEITUP_TOPIC_KEY } from '$env/static/private';
+
+
+async function SendEvent(): Promise<void> {
+    const client = new EventGridPublisherClient(
+        TEEITUP_TOPIC_ENDPOINT,
+        "CloudEvent",
+        new AzureKeyCredential(TEEITUP_TOPIC_KEY)
+    );
+    await client.send([
+        {
+            type: "azure.sdk.eventgrid.samples.cloudevent",
+            source: "/azure/sdk/eventgrid/samples/sendEventSample",
+            data: {
+                message: "this is a sample event"
+            }
+        }
+    ]);
+}
 
 export const POST: RequestHandler = async ({ request }) => {
+    SendEvent().catch((err)=> {
+        console.error("Event sending error:", err)
+    })
+    console.log("After event sent")
     const { a, b } = await request.json();
     return json(a + b);
 };
